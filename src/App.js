9150ed6046e0b00d47006307133f7d6200b73921
lesia2022 <lesia.bezukh22@gmail.com>
2023-09-05 16:58:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+// App.js
+import React from 'react';
+import Card from './card';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>hello react</h1>
+            <Card/>
+        </div>
+    );
+}
+
+export default App;
+
+
